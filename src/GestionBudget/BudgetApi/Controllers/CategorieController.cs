1569b66214fa0da4b81c58b97df416a3c9fd304c
Geovani TSAGUE <gmtsague@gmail.com>
2023-02-16 07:45:31 +0100

    API (Catgéorie) - jout de commentaires concernant la documentation

--- src/GestionBudget/BudgetApi/Controllers/CategorieController.cs
+++ src/GestionBudget/BudgetApi/Controllers/CategorieController.cs
@@ -4,6 +4,10 @@ using Arch.EntityFrameworkCore.UnitOfWork;
 using DbEntities.Models;
 using Mapster;
 using System.Threading;
+using Microsoft.AspNetCore.Authorization;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+//using Microsoft.AspNetCore.Components;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +15,7 @@ namespace BudgetApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Basic")]
     public class CategorieController : ControllerBase
     {
         private readonly ILogger<CategorieController> _logger;
@@ -23,6 +28,10 @@ namespace BudgetApi.Controllers
         }
 
         // GET: api/<CategorieController>
+        /// <summary>
+        /// Affiche la liste des catégories d'opérations financières
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IEnumerable<CategorieDto>> GetAll()
         {
@@ -33,6 +42,12 @@ namespace BudgetApi.Controllers
         }
 
         // GET api/<CategorieController>/5
+        /// <summary>
+        /// Affiche une catégorie d'opération financière à partir de son Identifiant
+        /// </summary>
+        /// <param name="id">Identifiant de la catégorie</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<IActionResult>/*CategorieDto*/ Get(int id, CancellationToken cancellationToken)
         {
@@ -49,8 +64,14 @@ namespace BudgetApi.Controllers
         }
 
         // POST api/<CategorieController>
+        /// <summary>
+        /// Créer (Ajoute) une nouvelle catégorie d'opération
+        /// </summary>
+        /// <param name="dtoValue">Objet représentant la nouvelle catégorie à créer</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] CategorieDto dtoValue, CancellationToken cancellationToken)
+        public async Task<IActionResult> Post([FromBody, Required] CategorieDto dtoValue, CancellationToken cancellationToken)
         {
             if (!ModelState.IsValid)
             {
@@ -68,6 +89,12 @@ namespace BudgetApi.Controllers
         }
 
         // PUT api/<CategorieController>/5
+        /// <summary>
+        /// Mettre à jour une catégorie
+        /// </summary>
+        /// <param name="id">Identifiant de la catégorie à modifier</param>
+        /// <param name="dtoValue">Objet representant les nouvelles valeurs de la catgéorie</param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] CategorieDto dtoValue)
         {
@@ -91,6 +118,11 @@ namespace BudgetApi.Controllers
         }
 
         // DELETE api/<CategorieController>/5
+        /// <summary>
+        /// Supprimer une catégorie
+        /// </summary>
+        /// <param name="id">Identifiant de la catégorie à supprimer</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
