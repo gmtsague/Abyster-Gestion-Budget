994fd97e5d717c0d8891ad1d11204afb00b1f7c6
Geovani TSAGUE <gmtsague@gmail.com>
2023-02-16 07:48:13 +0100

    Ajout de l'attribut [Required] aux champs de la classe

--- src/GestionBudget/BudgetApi/Dto/CategorieDto.cs
+++ src/GestionBudget/BudgetApi/Dto/CategorieDto.cs
@@ -1,10 +1,13 @@
 ﻿using DbEntities.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace BudgetApi.Dto
 {
     public class CategorieDto : BaseDto<CategorieDto, Categorie>
     {
+        [Required] 
         public int Idcategorie { get; set; }
+        [Required]
         public string Libelle { get; set; } = null!;
     }
 }
